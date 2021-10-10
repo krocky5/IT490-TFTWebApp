1b7f1df39df409c842aa5aa2e90e84e64c1fc047
Jeanpierre Rea <70703943+jeanpierrerea@users.noreply.github.com>
2021-10-10 14:51:12 -0400

    Update stop.sh
    typo change

--- Composer Scripts/stop.sh
+++ Composer Scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-ssh -t -p 2222 'jp@172.24.122.108' sudo systemctl stop  rabbitmq-server.service
+ssh -t -p 2222 'jp@172.24.122.108' sudo systemctl stop rabbitmq-server.service
 
 ssh -t minnie@172.24.151.237 sudo service apache2 stop
 
